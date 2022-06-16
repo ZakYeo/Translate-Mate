658ce6e25c9b5749543f8ca589bf9ebf5ff33e00
ZakYeo <zakyeomanson@gmail.com>
2022-06-16 16:00:27 +0100

    Added asynchronous usage & translate functions

---
+++ asyncdeepl.py
@@ -0,0 +1,23 @@
+import aiohttp
+
+API_URL = r"https://api-free.deepl.com/v2/"
+
+async def get_usage(key):
+    data={
+            "auth_key": key,
+        }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(API_URL+'usage', params=data) as resp:
+            text = await resp.text()
+            return (resp.status, text)
+
+async def translate(key, lang, text):
+    data= {
+        "target_lang": lang,
+        "auth_key": key,
+        "text": text,
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(API_URL+'translate', params=data) as resp:
+            text = await resp.text()
+            return (resp.status, text)
