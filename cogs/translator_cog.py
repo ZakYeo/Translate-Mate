42969c2915a09bd102760f12be680cbef155882e
ZakYeo <zakyeomanson@gmail.com>
2022-08-03 14:59:33 +0100

    Refactored code into a cog format for better interoperability, compatability and readability

---
+++ cogs/translator_cog.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+from discord.commands import Option
+import asyncdeepl as deepl
+from discord import Embed, Color
+
+class Translator(commands.Cog):
+
+    def __init__(self, bot): 
+        self.bot = bot
+    
+    @discord.slash_command(name="translate", description="Translate text!")
+    async def translate(self,
+        ctx,
+        text_original: Option(str, "Enter the text you wish to translate", required=True),
+        language: Option(str, "Select The Language To Translate To (Blank for English-GB)",choices=["en-gb","de","fr"], required=False)
+        ):
+        status, text_translated = await deepl.translate(self.bot.auth_key, language, text_original)
+
+        if status == 200:
+            embed = Embed(title="{} to {}".format(text_translated["translations"][0]["detected_source_language"],language)
+                        ,description="", color=Color.green())
+            embed.add_field(name="Original Text", value=text_original)
+            embed.add_field(name="Translated Text", value=text_translated["translations"][0]["text"])
+        else:
+            embed = Embed(title="Error {}".format(status), description=text_translated["message"],
+            color=Color.red())
+        await ctx.respond(embed=embed)
+
+    @discord.slash_command(name="usage", description="Check usage remaining!")
+    async def usage(self, ctx):
+        status, text = await deepl.get_usage(self.bot.auth_key)
+        if status == 200:
+            embed = Embed(title="DeepL's API Usage Statistics",description="", color=Color.green())
+            embed.add_field(name="Character Count", value=text["character_count"])
+            embed.add_field(name="Character Limit", value=text["character_limit"])
+        else:
+            embed = Embed(title="Error {}".format(status), description=text["message"],
+            color=Color.red())
+
+        await ctx.respond(embed=embed)
+
+
+
+def setup(bot): # this is called by Pycord to setup the cog
+    bot.add_cog(Translator(bot)) # add the cog to the bot
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import asyncdeepl as deepl
-from discord.ext import commands, tasks
-from discord.commands import Option
-from discord import Embed, Color
+from discord.ext import commands
 
 bot = commands.Bot(command_prefix='.')
-bot.auth_key = ""
-
+bot.auth_key = "" # Insert your DeepL auth key here
+token = "" # Insert your Discord bot key here
 
 @bot.event
 async def on_ready():
@@ -14,39 +11,19 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-
-@bot.slash_command(name="translate", description="Translate text!")
-async def translate(
-    ctx,
-    text_original: Option(str, "Enter the text you wish to translate", required=True),
-    language: Option(str, "Select The Language To Translate To (Blank for English-GB)",choices=["en-gb","de","fr"], required=False)
-    ):
-    if language is None:
-        language = "en-gb"
-    status, text_translated = await deepl.translate(bot.auth_key, language, text_original)
-
-    if status == 200:
-        embed = Embed(title="{} to {}".format(text_translated["translations"][0]["detected_source_language"],language)
-                    ,description="", color=Color.green())
-        embed.add_field(name="Original Text", value=text_original)
-        embed.add_field(name="Translated Text", value=text_translated["translations"][0]["text"])
-    else:
-        embed = Embed(title="Error {}".format(status), description=text_translated["message"],
-        color=Color.red())
-    await ctx.respond(embed=embed)
-
-@bot.slash_command(name="usage", description="Check usage remaining!")
-async def usage(ctx):
-    status, text = await deepl.get_usage(bot.auth_key)
-    if status == 200:
-        embed = Embed(title="DeepL's API Usage Statistics",description="", color=Color.green())
-        embed.add_field(name="Character Count", value=text["character_count"])
-        embed.add_field(name="Character Limit", value=text["character_limit"])
-    else:
-        embed = Embed(title="Error {}".format(status), description=text["message"],
-        color=Color.red())
-
-    await ctx.respond(embed=embed)
+    
     
 if __name__ == "__main__":
-    bot.run("")
+    from os import listdir
+
+    # Load any cogs in the cog directory
+    print("Loading Cogs Please Wait...")
+    for cog in listdir("cogs"):
+        if(cog == "__pycache__"):
+            pass
+        else:
+            bot.load_extension("cogs."+cog[:-3]) # :-3 to remove file extension (.py)
+
+    print("Cogs Loaded, success!")
+    bot.run(token)
+    
